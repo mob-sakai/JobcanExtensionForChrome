854a382aff508ae63805d7c697e1bdeae8e29040
mob-ichihara <ichihara@mobcast.jp>
2017-07-07 10:38:23 +0900

    定数追加忘れ

--- JobcanExtension/scripts/common/constants.js
+++ JobcanExtension/scripts/common/constants.js
@@ -1,6 +1,7 @@
 const Constants = {
   STORAGE_KEY: {
     TARGET_YEAR: "manHourManage-targetYear",
-    TARGET_MONTH: "manHourManage-targetMonth"
+    TARGET_MONTH: "manHourManage-targetMonth",
+    EASY_INPUT_VALUE: "easy-input-value"
   }
 };
